8221e2e7c6fd3f1ca822cf6360365d90cfa2619d
tridipdowari <tridipdowari75@gmail.com>
2025-08-11 23:54:50 +0530

    02_MiddleOfTheLinkedList.cpp

---
+++ LinkedList/02_MiddleOfTheLinkedList.cpp
@@ -0,0 +1,49 @@
+// Problem: Middle of the Linked List
+// Link: https://leetcode.com/problems/middle-of-the-linked-list/
+// Difficulty: Easy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Example linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    ListNode* middle = solution.middleNode(head);
+    cout << "Middle node value: " << middle->val << endl;
+
+    return 0;
+}
